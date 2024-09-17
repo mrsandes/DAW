6146f000eb5e35769d9297e15806f91c6f94f0b9
Marco Túlio Sandes Costa <mtsandes@yahoo.com>
2024-09-17 12:04:12 -0300

    menu da lista

---
+++ Lista1/menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Menu lista 1</title>
+</head>
+
+<body>
+    <h2>Menu</h2>
+
+    <select autofocus="" onchange="window.open(this.value, '', '')">
+        <option value="menu.php">Menu</option>
+        <option value="1D.php">Exercicio 1</option>
+        <option value="2D.php">Exercicio 2</option>
+        <option value="3D.php">Exercicio 3</option>
+        <option value="4D.php">Exercicio 4</option>
+        <option value="5.php">Exercicio 5</option>
+        <option value="6.php">Exercicio 6</option>
+        <option value="7.php">Exercicio 7</option>
+        <option value="8D.php">Exercicio 8</option>
+        <option value="9.php">Exercicio 9</option>
+        <option value="10.php">Exercicio 10</option>
+        <option value="11.php">Exercicio 11</option>
+        <option value="12.php">Exercicio 12</option>
+        <option value="13.php">Exercicio 13</option>
+    </select>
+</body>
+</html>
